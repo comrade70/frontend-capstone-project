f8c7771356311f703af91c4fe67b95259bf11827
comrade70 <ademolajoseph88@gmail.com>
2023-03-15 13:29:28 +0100

    some components files updated

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,21 +1,29 @@
+import React, {useState} from "react"
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaRegTimesCircle} from 'react-icons/fa'
 import Logo from '../../assets/Logo.svg'
-import React from "react"
-//import '../../App.css';
 import './navbar.css';
 
 function Navbar() {
+
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
   return (
     <nav className="navbar">
       <div className="container">
         <img src={Logo} alt="Logo" />
-        <ul className='nav-menu'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Menu</li>
-          <li>Reservation</li>
-          <li>Order Online</li>
-          <li>Login</li>
+        <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Reservation</a></li>
+          <li><a href="#">Order Online</a></li>
+          <li><a href="#">Login</a></li>
         </ul>
+        <div className="hamburger" onClick ={handleClick}>
+          {click ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon'/>)}
+        </div>
       </div>
     </nav>
   );
